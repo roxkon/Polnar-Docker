cf6e5392785c8e12159c8055bba2e2d04ca96714
mateuszmitek <mateusz.mitek95@gmail.com>
2018-10-03 10:10:39 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ class App extends React.Component<{}, IState> {
   }
 
   public async componentDidMount() {
-    const response = await axios.get('http://localhost:3001/greetings/hello',{
+    const response = await axios.get('http://localhost:3001/greetings/hello', {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -38,32 +38,32 @@ class App extends React.Component<{}, IState> {
     return (
       <div className="App">
         <header className="App-header">
-        <Header />
+          <Header />
         </header>
         <div className="App-main">
-        <Carousel>
-  <Carousel.Item>
-    <img width={"100%"} alt="900x500" src={IMG} />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img width={"100%"} height={500} alt="900x500" src={IMG} />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img width={"100%"} height={500} alt="900x500" src={IMG} />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>;
+          <Carousel>
+            <Carousel.Item>
+              <img width={"100%"} alt="900x500" src={IMG} />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width={"100%"} height={500} alt="900x500" src={IMG} />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width={"100%"} height={500} alt="900x500" src={IMG} />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>;
           {this.state.content}
         </div>
       </div>
--- frontend/src/components/HeaderNav/index.tsx
+++ frontend/src/components/HeaderNav/index.tsx
@@ -10,8 +10,6 @@ interface IState {
   lgShowSignUp?: boolean;
   lgOpenSignIn?: boolean;
   lgOpenSignUp?: boolean;
-  
-  
 }
 
 interface IProps {
@@ -19,7 +17,7 @@ interface IProps {
 }
 
 
- class HeaderNav extends React.Component<IProps,IState> {
+class HeaderNav extends React.Component<IProps, IState> {
   public constructor(props: IProps) {
     super(props);
 
@@ -38,57 +36,55 @@ interface IProps {
     this.setState({ lgShowSignUp: true })
   }
 
-    public render() {
-      const lgCloseSignIn = () => this.setState({ lgShowSignIn: false })
-      const lgCloseSignUp = () => this.setState({ lgShowSignUp: false })
-        return (
-          <div className="container clearfix">
-          <div className="one-third column">
-     
-            <div className="logo">
+  public render() {
+    const lgCloseSignIn = () => this.setState({ lgShowSignIn: false })
+    const lgCloseSignUp = () => this.setState({ lgShowSignUp: false })
+    return (
+      <div className="container clearfix">
+        <div className="one-third column">
+
+          <div className="logo">
             <a className="scroll" href="#wrapper"><h1>more<span>+</span></h1></a>
-            </div>
           </div>
-          
-          
-            <div className="two-thirds column">
-        
-                  <nav id="mainNav">
-                    <a href="#" className="mobileBtn"><i className="icon-menu"/></a>
-    
-                  <ul>
-                    <li><a className="scroll" href="#about">About us</a></li>
-                    <li><a className="scroll" href="#services">Services</a></li>
-                    <li><a className="scroll" href="#portfolio">Portfolio</a></li>
-                    <li><a className="scroll" href="#blog">BLog</a></li>
-                    <li><a className="scroll" href="#contact">Contact</a></li>
-                    <li>
-                      <ButtonToolbar>
-                      <Button
-                      bsClass="myButton"
-                      onClick={this.lgOpenSignIn}>
-                      Zaloguj!
+        </div>
+        <div className="two-thirds column">
+
+          <nav id="mainNav">
+            <a href="#" className="mobileBtn"><i className="icon-menu" /></a>
+
+            <ul>
+              <li><a className="scroll" href="#about">About us</a></li>
+              <li><a className="scroll" href="#services">Services</a></li>
+              <li><a className="scroll" href="#portfolio">Portfolio</a></li>
+              <li><a className="scroll" href="#blog">BLog</a></li>
+              <li><a className="scroll" href="#contact">Contact</a></li>
+              <li>
+                <ButtonToolbar>
+                  <Button
+                    bsClass="myButton"
+                    onClick={this.lgOpenSignIn}>
+                    Zaloguj!
                       </Button>
-                      <Button
-                      bsClass="myButton"
-                      onClick={this.lgOpenSignUp}>
-                      Rejestracja!
+                  <Button
+                    bsClass="myButton"
+                    onClick={this.lgOpenSignUp}>
+                    Rejestracja!
                       </Button>
-                      <LogInModal show={this.state.lgShowSignIn} onHide={lgCloseSignIn} />
-                      <SignUpModal show={this.state.lgShowSignUp} onHide={lgCloseSignUp} />
-                      </ButtonToolbar>
-                      </li>
-                    {/* <li><button className="myButton" bsStyle="primary" onClick={() => this.setState({ lgShow: true })}>sign in</button></li> */}
-                  </ul>
-                  
-                  </nav>
-                  
-      
-            </div>
-          </div>
-       
-        );
-      }
+                  <LogInModal show={this.state.lgShowSignIn} onHide={lgCloseSignIn} />
+                  <SignUpModal show={this.state.lgShowSignUp} onHide={lgCloseSignUp} />
+                </ButtonToolbar>
+              </li>
+              {/* <li><button className="myButton" bsStyle="primary" onClick={() => this.setState({ lgShow: true })}>sign in</button></li> */}
+            </ul>
+
+          </nav>
+
+
+        </div>
+      </div>
+
+    );
+  }
 }
 
 export default HeaderNav;
